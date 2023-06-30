e8b7aaf46b19f9ae652e118857a44abcd2d3190a
dabhisharad13 <dabhisharad13@gmail.com>
2023-06-30 15:14:53 -0700

    Added Empty components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Username from "./components/Username";
+import Reset from "./components/Reset";
+import Register from "./components/Register";
+import Recovery from "./components/Recovery";
+import Profile from "./components/Profile";
+import Password from "./components/Password";
+import PageNotFound from "./components/PageNotFound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Username />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/reset" element={<Reset />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/recovery" element={<Recovery />} />
+          <Route path="/password" element={<Password />} />
+          <Route path="/pagenotfound" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </main>
   );
 }
 
---
+++ frontend/src/components/Reset.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const REset = () => {
+  return (
+    <div>REset</div>
+  )
+}
+
+export default REset
