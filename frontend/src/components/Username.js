7fa1a689699918cfb4c66d98d45c8418956f20ea
dabhisharad13 <dabhisharad13@gmail.com>
2023-06-30 16:01:45 -0700

    Added username layout

--- frontend/src/components/Username.js
+++ frontend/src/components/Username.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Username = () => {
   return (
-    <div>Username</div>
-  )
-}
+    <div className="container mx-auto">
+      <div className="flex justify-center items-center h-screen">
+        <div>
+          <div className="title flex flex-col items-center">
+            <h4 className="text-5xl font-bold"> Hello!</h4>
+            <span className="py-4 text-xl w-2/3 text-center text-gray-500">
+              hfnjkdsnkfndskjfn dkfndskjfn kjsdnfkjdsn fkje
+            </span>
+          </div>
 
-export default Username
+          <form className="py-1" action="">
+            <div className="profile flex justify-center py-4">
+              <img src="" alt="avatar" />
+            </div>
+
+            <div className="textbox flex flex-col items-center gap-6">
+              <input type="text" placeholder="Username" />
+              <button type="submit">Submit</button>
+            </div>
+
+            <div className="text-center py-4">
+              <span className="text-gray-500">
+                Not a Member?
+                <Link className="text-red-500" to="/register">
+                  Register Now!
+                </Link>
+              </span>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Username;
