92021c814c2f420b401dc79aacf8b56fd7783ffa
dabhisharad13 <dabhisharad13@gmail.com>
2023-06-30 16:59:03 -0700

    Added css styles for username

--- frontend/src/components/Username.js
+++ frontend/src/components/Username.js
@@ -1,26 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Avatar from "../assets/avatar.png"
+import styles from "../styles/Username.module.css"
 
 const Username = () => {
   return (
     <div className="container mx-auto">
       <div className="flex justify-center items-center h-screen">
-        <div>
+        <div className={styles.glass}>
           <div className="title flex flex-col items-center">
             <h4 className="text-5xl font-bold"> Hello!</h4>
             <span className="py-4 text-xl w-2/3 text-center text-gray-500">
-              hfnjkdsnkfndskjfn dkfndskjfn kjsdnfkjdsn fkje
+              Let's connect
             </span>
           </div>
 
           <form className="py-1" action="">
             <div className="profile flex justify-center py-4">
-              <img src="" alt="avatar" />
+              <img className= {styles.profile_img}src={Avatar} alt="avatar" />
             </div>
 
             <div className="textbox flex flex-col items-center gap-6">
-              <input type="text" placeholder="Username" />
-              <button type="submit">Submit</button>
+              <input className={styles.textbox}type="text" placeholder="Username" />
+              <button className={styles.btn}type="submit">Submit</button>
             </div>
 
             <div className="text-center py-4">
