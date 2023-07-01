be466cccec98afeb42a6e6e3b08fd2ea86cd591a
dabhisharad13 <dabhisharad13@gmail.com>
2023-06-30 21:00:13 -0700

    Added recovery component

--- frontend/src/components/Recovery.js
+++ frontend/src/components/Recovery.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+import styles from "../styles/Style.module.css";
 
 const Recovery = () => {
   return (
-    <div>Recovery</div>
-  )
-}
+    <div className="container mx-auto">
+      <div className="flex justify-center items-center h-screen">
+        <div className={styles.glass}>
+          <div className="title flex flex-col items-center">
+            <h4 className="text-5xl font-bold"> Recovery</h4>
+          </div>
 
-export default Recovery
+          <form className="pt-20" action="">
+            <div className="textbox flex flex-col items-center gap-6">
+              <div className="input text-center">
+                <span className="py-4 text-sm text-left text-gray-500">
+                  Enter 6 digit OTP sent to your email.
+                </span>
+                <input
+                  className={styles.textbox}
+                  type="text"
+                  placeholder="Enter OTP"
+                />
+              </div>
+
+              <button className={styles.btn} type="submit">
+                Submit
+              </button>
+            </div>
+
+            <div className="text-center py-4">
+              <span className="text-gray-500">
+                Didn't receive OTP?{" "}
+                <button className="text-red-500">Resend</button>
+              </span>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Recovery;
